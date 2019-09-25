6fff6d11555a519855bbb7d2d0c87276321bf802
sdasov <sdasov@gmail.com>
2019-09-25 22:32:36 +0300

    Added class KeyboardDriver

---
+++ src/KeyboardDriver.cpp
@@ -0,0 +1,22 @@
+/*
+ * KeyboardDriver.cpp
+ *
+ *  Created on: 25 сент. 2019 г.
+ *      Author: sdaso
+ */
+
+#include "KeyboardDriver.h"
+
+KeyboardDriver::KeyboardDriver() {currentPtr=0;}
+
+KeyboardDriver::~KeyboardDriver() {}
+
+void KeyboardDriver::setKey(char key)
+{
+
+}
+
+char KeyboardDriver::getKey()
+{
+	return 0;
+}
---
+++ src/KeyboardDriver.h
@@ -0,0 +1,22 @@
+/*
+ * KeyboardDriver.h
+ *
+ *  Created on: 25 сент. 2019 г.
+ *      Author: sdaso
+ */
+
+#ifndef KEYBOARDDRIVER_H_
+#define KEYBOARDDRIVER_H_
+
+class KeyboardDriver {
+public:
+	KeyboardDriver();
+	~KeyboardDriver();
+	void setKey(char key);
+	char getKey();
+private:
+	char keysBuffer[16];
+	unsigned char currentPtr;
+};
+
+#endif /* KEYBOARDDRIVER_H_ */
